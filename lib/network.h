46ca9f53a0c70f59b90089e16d2bd50af0fdb9b3
Superboy <zakharenkolink@gmail.com>
2024-05-05 19:11:33 +0300

    Perceptron graph rework need to lern bias theme, mb change a structure of project

--- lib/network.h
+++ lib/network.h
@@ -7,25 +7,36 @@ namespace s21
 {
 
 #define NETWORK_SENSOR_SIZE 144
-#define NETWORK_HIDDEN_SIZE 500
+#define NETWORK_HIDDEN_COUNT 2
+#define NETWORK_HIDDEN_SIZE 240
 #define NETWORK_OUTPUT_SIZE 26
 
 class Network {
 public:
-    
+    Network() {
+        sensor_ = new SensorPerceptron[NETWORK_SENSOR_SIZE];
+        hidden_ = new HiddenPerceptron*[NETWORK_HIDDEN_COUNT];
+        output_ = new OutputPerceptron[NETWORK_OUTPUT_SIZE];
+        for (size_t layer = 0; layer < NETWORK_HIDDEN_COUNT; ++layer) {
+            hidden_[layer] = new HiddenPerceptron[NETWORK_HIDDEN_SIZE];
+        }
+    };
+    ~Network() noexcept {
+        delete[] sensor_;
+        for (size_t layer = 0; layer < NETWORK_HIDDEN_COUNT; ++layer) {
+            delete hidden_[layer];
+        }
+        delete[] hidden_;
+        delete[] output_;
+    };
 
-    Network() {};
-    ~Network() noexcept {};
+    void loadData(); // геттер для будующего фасада
+
+    vo
 
 private:
     BasePerceptron* sensor_;
-
-    BasePerceptron* hiden_layer_1_;
-    BasePerceptron* hiden_layer_2_;
-    BasePerceptron* hiden_layer_3_;
-    BasePerceptron* hiden_layer_4_;
-    BasePerceptron* hiden_layer_5_;
-
+    BasePerceptron** hidden_;
     BasePerceptron* output_;
 };
 
--- lib/perceptron/perceptron.h
+++ lib/perceptron/perceptron.h
@@ -6,7 +6,7 @@
 
 namespace s21 {
 
-#define PERCEPTRON_BIAS 0.0 // честно хз пока куда это засунуть, так как макросы плохо использовать
+#define PERCEPTRON_BIAS 1.0 // честно хз пока куда это засунуть, так как макросы плохо использовать
 #define PERCEPTRON_ACTIVATION_LEVEL 0.7
 #define PERCEPTRON_WEIGHT 1.0
 
@@ -15,112 +15,73 @@ using weight_type = double;
 
 class BasePerceptron {
 public:
-    virtual ~BasePerceptron() noexcept = 0;
-    virtual void handle() = 0;
-    virtual perceptron_general_activation_type getVal() const noexcept = 0;
-};
-
-class SensorPerceptron : BasePerceptron {
-public:
-    SensorPerceptron() {
-        output_ptr_ = nullptr;
+    BasePerceptron() {
+        input_ptrs_.clear();
         val_ = 0.0;
     };
 
-    ~SensorPerceptron() noexcept override {
-        output_ptr_ = nullptr;
-        val_ = 0.0;
-    };
-
-    void setOuputConnection(HiddenPerceptron* perc) noexcept {
-        this->output_ptr_ = perc;
+    virtual ~BasePerceptron() noexcept {
+        this->val_ = 0.0;
+        input_ptrs_.clear();
     };
     
+    virtual void handle() = 0;
+    
+    perceptron_general_activation_type getVal() const noexcept {
+        return this->val_;
+    };
+
     void setVal(perceptron_general_activation_type val) noexcept {
         this->val_ = val;
-    };
-
-    perceptron_general_activation_type getVal() const noexcept override {
-        return this->val_;
-    };
-
-    void handle() override {
-        ActivationRules::sigmoid(this->val_, PERCEPTRON_BIAS);
-        if (this->val_ <= PERCEPTRON_ACTIVATION_LEVEL) {
-            output_ptr_->addConnection(this, PERCEPTRON_WEIGHT);
-        }
-    };
-
-private:
-    perceptron_general_activation_type val_;
-    HiddenPerceptron *output_ptr_;
-};
-
-class HiddenPerceptron : BasePerceptron {
-public:
-    HiddenPerceptron() {
-        output_ptr_ = nullptr;
-        input_ptrs_.clear();
-        val_ = 0.0;
-    };
-
-    ~HiddenPerceptron() noexcept override {
-        val_ = 0.0;
-        input_ptrs_.clear();
-    };
-
-    perceptron_general_activation_type getVal() const noexcept override {
-        return this->val_;
     };
 
     void addConnection(BasePerceptron* p, const weight_type& weight_of_connection) {
         input_ptrs_.insert(std::make_pair(p, weight_of_connection));
     };
 
+protected:
+    perceptron_general_activation_type val_;
+    std::map<BasePerceptron*, weight_type> input_ptrs_;
+};
+
+class HiddenPerceptron : public BasePerceptron {
+public:
     void handle() override {
         for (auto& i_perc : input_ptrs_) {
             this->val_ += i_perc.first->getVal() * i_perc.second;
         }
         ActivationRules::sigmoid(this->val_, PERCEPTRON_BIAS);
         if (this->val_ <= PERCEPTRON_ACTIVATION_LEVEL) {
-            output_ptr_->addConnection(this, PERCEPTRON_WEIGHT);
+            val_ = 0.0;
         }
     };
-
-private:
-    HiddenPerceptron *output_ptr_;
-    std::map<BasePerceptron*, weight_type> input_ptrs_;
-    perceptron_general_activation_type val_;
-    
 };
 
-class OutputPerceptron : BasePerceptron {
+
+class SensorPerceptron : public BasePerceptron {
 public:
-    OutputPerceptron() {
-        input_ptrs_.clear();
-        val_ = 0.0;
+    void addConnection(BasePerceptron* p, const weight_type& weight_of_connection) = delete;
+    
+    void handle() override {
+        ActivationRules::sigmoid(this->val_, PERCEPTRON_BIAS);
+        if (this->val_ <= PERCEPTRON_ACTIVATION_LEVEL) {
+            val_ = 0.0;
+        }
     };
+};
 
-    ~OutputPerceptron() noexcept override {
-        val_ = 0.0;
-        input_ptrs_.clear();
-    };
 
-    perceptron_general_activation_type getVal() const noexcept override {
-        return this->val_;
-    };
-
+class OutputPerceptron : public BasePerceptron {
+public:
     void handle() override {
         for (auto& i_perc : input_ptrs_) {
             this->val_ += i_perc.first->getVal() * i_perc.second;
         }
         ActivationRules::sigmoid(this->val_, PERCEPTRON_BIAS);
+        if (this->val_ <= PERCEPTRON_ACTIVATION_LEVEL) {
+            val_ = 0.0;
+        }
     };
-
-private:
-    std::map<HiddenPerceptron*, weight_type> input_ptrs_;
-    perceptron_general_activation_type val_;
-
 };
 
 }; // s21
